54410a7ef6fd5fefe716fe20a1bdf359b0adae23
Jēy Löuie <jeylouie@JeyBook-Pro.local>
2022-07-30 15:31:47 -0700

    adding updates

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,30 +1,31 @@
- import React from 'react';
- import { withRouter } from 'react-router-dom';
-import NotFound from './NotFound';
- import Photo from './Photo';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import NotFound from "./NotFound";
+import Photo from "./Photo";
 
- 
- const PhotoContainer = (props) => {
-   
-    let photos = props.data.map(photo => 
-        <Photo
-            url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-            key={photo.id}
-        />
-    );
+const PhotoContainer = (props) => {
+    console.log(props.data);
+  let photos = props.data.map((photo) => (
+    <Photo
+      url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+      key={photo.id}
+    />
+  ));
 
-    
-return(
-<div className="photo-container">
-    {(props.loading) ? <p>Loading</p> :
-    (!props.data.length && !props.loading) ? <NotFound/> :
-    <div>
-        <h2> Image Of: {props.title} </h2>
-        <ul> {photos} </ul>
+  return (
+    <div className="photo-container">
+      {props.loading ? (
+        <p>Loading</p>
+      ) : !props.data.length && !props.loading ? (
+        <NotFound />
+      ) : (
+        <div>
+          <h2> Image Of: {props.title} </h2>
+          <ul> {photos} </ul>
+        </div>
+      )}
     </div>
-    }
-</div>
-);
-}
+  );
+};
 
 export default withRouter(PhotoContainer);
